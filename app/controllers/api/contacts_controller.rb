e0800aa701aad4c4b9e1283b4a1c754a23843f03
Brandon Evans <brandon_n_evans@live.com>
2019-06-29 22:42:56 -0500

    add contact index and show action + routes

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,30 @@
 class Api::ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+    render "index.json.jbuilder"
+  end
+
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      phone_number: params[:phone_number],
+      email: params[:email]
+    )
+    @contact.save
+    render "show.json.jbuilder"
+  end
+
+  def update
+    @contact
+  end
+
+  def delete
+    @contact
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    get '/contacts' => 'contacts#index'
+    get '/contacts/:id' => 'contacts#show'
+
+    get '/posts' => 'posts#index'
+  end
 end
